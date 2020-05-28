ef2e6c7c11c0b26a6c626d63eb259e3dff94ca3d
GeunheeHan <64899272+GeunheeHan@users.noreply.github.com>
2020-05-28 23:05:26 +0900

    Update charpointer.c

--- Ch11/charpointer.c
+++ Ch11/charpointer.c
@@ -6,18 +6,16 @@ int main(void)
 	char *java = "java";
 	printf("%s ", java);
 
-	//문자 포인터가 가리키는 문자 이후를 하나 하나 출력
 	int i = 0;
-	while (java[i]) //while (java[i] != '\0')
+	while (java[i]) 
 		printf("%c", java[i++]);
 	printf(" ");
 
 	i = 0;
-	while (*(java + i) != '\0') //java[i]는 *(java + i)와 같음
+	while (*(java + i) != '\0') 
 		printf("%c", *(java + i++));
 	printf("\n");
 
-	//수정 불가능, 실행오류 발생 
 	java[0] = 'J';
 
 	return 0;
