0c609fb0942ad5427ff6771b98fe2633d0dae1a1
GeunheeHan <64899272+GeunheeHan@users.noreply.github.com>
2020-05-28 23:23:23 +0900

    Update strcpy.c

--- Ch11/strcpy.c
+++ Ch11/strcpy.c
@@ -1,4 +1,3 @@
-// file: strcpy.c 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
@@ -9,13 +8,9 @@ int main(void)
 	char source[80] = "C is a language.";
 
 	printf("%s\n", strcpy(dest, source));
-	//printf("%d\n", strcpy_s(dest, 80, source));
-	//printf("%s\n", dest);
 	printf("%s\n", strncpy(dest, "C#", 2));
 
 	printf("%s\n", strncpy(dest, "C#", 3));
-	//printf("%d\n", strncpy_s(dest, 80, "C#", 3));
-	//printf("%s\n", dest);
 
 	return 0;
 }
